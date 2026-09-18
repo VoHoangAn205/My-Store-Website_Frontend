8fc18821c0dcf22d123007f06492385c4947758f
HgAn <hoanganvo1812@gmail.com>
2026-09-18 13:35:39 +0700

    new backend url connection

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://hoangan-store-918.vercel.app/";
 
 export const axiosPublic = axios.create({
   baseURL: BASE_URL,
